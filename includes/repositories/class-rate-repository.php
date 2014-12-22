807ce7dff097b495d60a5f5ee85b3050a721c345
Coen Jacobs <coenjacobs@gmail.com>
2014-12-22 14:28:00 +0100

    Disable type parameter in API calls

--- includes/repositories/class-rate-repository.php
+++ includes/repositories/class-rate-repository.php
@@ -3,24 +3,26 @@
 class VIEU_Rate_Repository extends VIEU_Abstract_Repository {
 	private $rates = array();
 
-	public function get_rates( $country_id, $type_id, $category_id = null ) {
+//	public function get_rates( $country_id, $type_id, $category_id = null ) {
+	public function get_rates( $country_id, $category_id = null ) {
 		if ( empty( $this->rates ) ) {
-			$this->rates = $this->query_rates( $country_id, $type_id, $category_id );
+			$this->rates = $this->query_rates( $country_id, $category_id );
+//			$this->rates = $this->query_rates( $country_id, $type_id, $category_id );
 		}
 
 		return $this->rates;
 	}
 
-	private function query_rates( $country_id, $type_id, $category_id = null ) {
+	private function query_rates( $country_id, $category_id = null ) {
 		$handler = new VIEU_WC_API_Handler();
 		$arguments = array(
 			'category' => intval( $category_id ),
 			'country' => intval( $country_id ),
 		);
 
-		if ( null !== $type_id ) {
-			$arguments['type'] = intval( $type_id );
-		}
+//		if ( null !== $type_id ) {
+//			$arguments['type'] = intval( $type_id );
+//		}
 
 		$data = $handler->handle_request( 'rate', $arguments );
 
