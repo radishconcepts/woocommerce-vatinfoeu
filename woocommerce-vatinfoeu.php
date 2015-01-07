80e52b5cec16c7f97bc0b110701e5817f61d2b77
Coen Jacobs <coenjacobs@gmail.com>
2015-01-07 07:54:48 +0100

    Version bump to 0.3.2

--- woocommerce-vatinfoeu.php
+++ woocommerce-vatinfoeu.php
@@ -5,7 +5,7 @@
  * Description: Integrates the vatinfo.eu API with your WooCommerce powered website
  * Author: Radish Concepts
  * Author URI: http://www.radishconcepts.com
- * Version: 0.3.1
+ * Version: 0.3.2
  */
 
 if ( ! class_exists( 'VAT_Info_EU' ) ) {
