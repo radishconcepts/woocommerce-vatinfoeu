576721bc8e5358772431dfd713fe62e8e6003464
Coen Jacobs <coenjacobs@gmail.com>
2015-01-12 09:49:23 +0100

    Check for valid API key before query for rate

--- includes/core/class-tax.php
+++ includes/core/class-tax.php
@@ -24,29 +24,32 @@ class VIEU_Tax {
 	}
 
 	public function matched_tax_rates( $matched_tax_rates, $country ) {
+		$api_key = get_option('vieu_api_key');
 		if ( 'yes' === get_option( 'vieu_enabled' ) ) {
-			$country_repo = new VIEU_Country_Repository();
-			$vieu_country = $country_repo->get_country_by_code( $country );
+			if ( false !== $api_key && ! empty( $api_key ) ) {
+				$country_repo = new VIEU_Country_Repository();
+				$vieu_country = $country_repo->get_country_by_code( $country );
 
-			// This method will return false when country is not found in the API
-			// In most cases this means that a country is not within the EU.
-			if ( false === $vieu_country ) {
-				return $matched_tax_rates;
+				// This method will return false when country is not found in the API
+				// In most cases this means that a country is not within the EU.
+				if ( false === $vieu_country ) {
+					return $matched_tax_rates;
+				}
+
+				$category_id = get_option( 'vieu_category', null );
+
+				$rate_calc = new VIEU_Rate();
+				$rate      = $rate_calc->get_rate( $vieu_country->id, $category_id );
+
+				$new_tax_rates[1] = array(
+					'rate'     => $rate->rate,
+					'label'    => 'Yo',
+					'shipping' => 'yes',
+					'compound' => 'no',
+				);
+
+				$matched_tax_rates = $new_tax_rates;
 			}
-
-			$category_id = get_option('vieu_category', null );
-
-			$rate_calc = new VIEU_Rate();
-			$rate = $rate_calc->get_rate( $vieu_country->id, $category_id );
-
-			$new_tax_rates[1] = array(
-				'rate' => $rate->rate,
-				'label' => 'Yo',
-				'shipping' => 'yes',
-				'compound' => 'no',
-			);
-
-			$matched_tax_rates = $new_tax_rates;
 		}
 
 		return $matched_tax_rates;
