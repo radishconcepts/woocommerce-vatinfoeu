1f9ada1e171c2d05bfe0d8b64b2b047e21fbe8a9
Coen Jacobs <coenjacobs@gmail.com>
2015-01-12 09:52:51 +0100

    Conditionally hook function instead of condition logic

--- includes/core/class-tax.php
+++ includes/core/class-tax.php
@@ -2,9 +2,14 @@
 
 class VIEU_Tax {
 	public function __construct() {
-		add_filter( 'woocommerce_rate_code', array( $this, 'rate_code' ), 10, 2 );
-		add_filter( 'pre_option_woocommerce_tax_based_on', array( $this, 'option_tax_based_on' ), 10, 1 );
-		add_filter( 'woocommerce_matched_tax_rates', array( $this, 'matched_tax_rates' ), 10, 2 );
+		$api_key = get_option('vieu_api_key');
+		if ( 'yes' === get_option( 'vieu_enabled' ) ) {
+			if ( false !== $api_key && ! empty( $api_key ) ) {
+				add_filter( 'woocommerce_rate_code', array( $this, 'rate_code' ), 10, 2 );
+				add_filter( 'pre_option_woocommerce_tax_based_on', array( $this, 'option_tax_based_on' ), 10, 1 );
+				add_filter( 'woocommerce_matched_tax_rates', array( $this, 'matched_tax_rates' ), 10, 2 );
+			}
+		}
 	}
 
 	public function rate_code( $code_string, $key ) {
@@ -24,34 +29,27 @@ class VIEU_Tax {
 	}
 
 	public function matched_tax_rates( $matched_tax_rates, $country ) {
-		$api_key = get_option('vieu_api_key');
-		if ( 'yes' === get_option( 'vieu_enabled' ) ) {
-			if ( false !== $api_key && ! empty( $api_key ) ) {
-				$country_repo = new VIEU_Country_Repository();
-				$vieu_country = $country_repo->get_country_by_code( $country );
+		$country_repo = new VIEU_Country_Repository();
+		$vieu_country = $country_repo->get_country_by_code( $country );
 
-				// This method will return false when country is not found in the API
-				// In most cases this means that a country is not within the EU.
-				if ( false === $vieu_country ) {
-					return $matched_tax_rates;
-				}
-
-				$category_id = get_option( 'vieu_category', null );
-
-				$rate_calc = new VIEU_Rate();
-				$rate      = $rate_calc->get_rate( $vieu_country->id, $category_id );
-
-				$new_tax_rates[1] = array(
-					'rate'     => $rate->rate,
-					'label'    => 'Yo',
-					'shipping' => 'yes',
-					'compound' => 'no',
-				);
-
-				$matched_tax_rates = $new_tax_rates;
-			}
+		// This method will return false when country is not found in the API
+		// In most cases this means that a country is not within the EU.
+		if ( false === $vieu_country ) {
+			return $matched_tax_rates;
 		}
 
-		return $matched_tax_rates;
+		$category_id = get_option( 'vieu_category', null );
+
+		$rate_calc = new VIEU_Rate();
+		$rate      = $rate_calc->get_rate( $vieu_country->id, $category_id );
+
+		$new_tax_rates[1] = array(
+			'rate'     => $rate->rate,
+			'label'    => 'Yo',
+			'shipping' => 'yes',
+			'compound' => 'no',
+		);
+
+		return $new_tax_rates;
 	}
 }
